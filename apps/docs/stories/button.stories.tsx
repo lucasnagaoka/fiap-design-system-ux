c60ba4cfeb1d713bf9c4c27d531416d79d1a7e01
Lucas Nagaoka <lucasnagaoka@gmail.com>
2025-05-10 18:48:34 -0300

    fix(button): fixing button export

--- apps/docs/stories/button.stories.tsx
+++ apps/docs/stories/button.stories.tsx
@@ -1,5 +1,6 @@
-import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '@fiap-ds/ui/button';
+
+import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Button> = {
   component: Button,
