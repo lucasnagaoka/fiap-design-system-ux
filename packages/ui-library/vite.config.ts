cfc89c2c13f5a957ac62934cfce21cca14a51bbb
Lucas Nagaoka <lucasnagaoka@gmail.com>
2025-05-16 15:27:05 -0300

    feat(ui library): adding functional UI library (#2)

---
+++ packages/ui-library/vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
+import { extname, relative, resolve } from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), libInjectCss(), dts({ include: ['lib'] })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob
+          .sync('lib/**/*.{ts,tsx}', {
+            ignore: ['lib/**/*.d.ts'],
+          })
+          .map((file) => [
+            // The name of the entry point
+            // lib/nested/foo.ts becomes nested/foo
+            relative('lib', file.slice(0, file.length - extname(file).length)),
+            // The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
+    },
+  },
+});
