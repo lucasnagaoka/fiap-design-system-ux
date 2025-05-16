41fa4bceabc13a9c7154a93a6a2cb24cb0792d1a
Lucas Nagaoka <lucasnagaoka@gmail.com>
2025-05-16 17:23:42 -0300

    feat(spacing tokens): adding spacing tokens

---
+++ packages/ui-library/lib/design-tokens/spacing/index.ts
@@ -0,0 +1,15 @@
+const FIAP_DS_SPACING = {
+  s0: '0px',
+  s1: '4px',
+  s2: '8px',
+  s3: '12px',
+  s4: '16px',
+  s5: '20px',
+  s6: '24px',
+  s7: '32px',
+  s8: '40px',
+  s9: '48px',
+  s10: '64px',
+};
+
+export { FIAP_DS_SPACING };
