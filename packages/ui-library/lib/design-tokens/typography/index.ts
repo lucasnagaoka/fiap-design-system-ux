d193aba15fd58f4cc3648534155f787282198704
Lucas Nagaoka <lucasnagaoka@gmail.com>
2025-05-16 19:48:35 -0300

    Feat/design tokens (#4)
    * feat(color tokens): adding color tokens
    
    * feat(spacing tokens): adding spacing tokens
    
    * feat(typography tokens): adding typography tokens
    
    * style(spacing tokens): removing unit
    
    * style(typography tokens): adding missing tokens

---
+++ packages/ui-library/lib/design-tokens/typography/index.ts
@@ -0,0 +1,34 @@
+const FIAP_DS_TYPOGRAPHY = {
+  h1: {
+    'font-size': '48px',
+    'line-height': '56px',
+    weight: '700',
+  },
+  h2: {
+    'font-size': '36px',
+    'line-height': '44px',
+    weight: '500',
+  },
+  h3: {
+    'font-size': '28px',
+    'line-height': '34px',
+    weight: '500',
+  },
+  p: {
+    'font-size': '16px',
+    'line-height': '24px',
+    weight: '400',
+  },
+  small: {
+    'font-size': '14px',
+    'line-height': '20px',
+    weight: '100',
+  },
+  button: {
+    'font-size': '16px',
+    'line-height': '16px',
+    weight: '500',
+  },
+};
+
+export { FIAP_DS_TYPOGRAPHY };
