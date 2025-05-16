17cfabacf5ae601f5213df4151647a38c7e48a33
Lucas Nagaoka <lucasnagaoka@gmail.com>
2025-05-16 17:34:25 -0300

    feat(typography tokens): adding typography tokens

---
+++ packages/ui-library/lib/design-tokens/typography/index.ts
@@ -0,0 +1,24 @@
+const FIAP_DS_TYPOGRAPHY = {
+  h1: {
+    'font-size': '48px',
+    'line-height': '56px',
+    weight: '700',
+  },
+  h2: {
+    'font-size': '36px',
+    'line-height': '44px',
+    weight: '500',
+  },
+  h3: {
+    'font-size': '28px',
+    'line-height': '34px',
+    weight: '500',
+  },
+  p: {
+    'font-size': '16px',
+    'line-height': '24px',
+    weight: '400',
+  },
+};
+
+export { FIAP_DS_TYPOGRAPHY };
