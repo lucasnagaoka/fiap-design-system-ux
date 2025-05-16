4f6c53fd738304cf86a40377e8fe5c178b6c6d09
Lucas Nagaoka <lucasnagaoka@gmail.com>
2025-05-16 15:44:27 -0300

    fix(ui library): removing non-functional UI library with tsup (#3)

--- packages/ui/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig((options) => ({
-  entryPoints: ['src/button/button.tsx'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  external: ['react'],
-  ...options,
-}));
