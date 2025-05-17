89da42eb864ac8b1fae9a1adeee95b4ae941cbab
MarcosPaesLeme <natmapls@hotmail.com>
2025-05-17 15:36:42 -0300

    feat: adding the home page

--- apps/demo-receipts/app/welcome/welcome.tsx
+++ apps/demo-receipts/app/welcome/welcome.tsx
@@ -1,96 +1,123 @@
-import logoDark from './logo-dark.svg';
-import logoLight from './logo-light.svg';
+import { useState } from 'react';
 import { Button } from '@fiap-design-system/ui';
+import logoLight from '../assets/image.png';
 
-export function Welcome() {
-  function handleClick() {
-    console.log('Button clicked');
-    alert('Button clicked');
-  }
-
-  return (
-    <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <Button onClick={handleClick}>Test</Button>
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
-          </div>
-        </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
-            </p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </main>
-  );
-}
-
-const resources = [
+const recipes = [
   {
-    href: 'https://reactrouter.com/docs',
-    text: 'React Router Docs',
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 20 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z"
-          strokeWidth="1.5"
-          strokeLinecap="round"
-        />
-      </svg>
-    ),
+    title: 'Receita bolo',
+    description: 'Bolo de cenoura',
+    image:
+      'https://imgs.search.brave.com/TNgoDPGds0ixlWL5IKt4eQvay2A2K0rrvCJ7Zs58nUc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaXRkZy5jb20u/YnIvaW1hZ2VzLzY0/MC00NDAvNmEwYjk2/NDczZTgzODY2ZDdi/MDZmMGZkYWE3MzY2/NmEvODQxODMtMzI5/NTE4LW9yaWdpbmFs/LmpwZy5qcGc',
   },
   {
-    href: 'https://rmx.as/discord',
-    text: 'Join Discord',
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="20"
-        viewBox="0 0 24 20"
-        fill="none"
-        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
-      >
-        <path
-          d="M15.0686 1.25995L14.5477 1.17423L14.2913 1.63578C14.1754 1.84439 14.0545 2.08275 13.9422 2.31963C12.6461 2.16488 11.3406 2.16505 10.0445 2.32014C9.92822 2.08178 9.80478 1.84975 9.67412 1.62413L9.41449 1.17584L8.90333 1.25995C7.33547 1.51794 5.80717 1.99419 4.37748 2.66939L4.19 2.75793L4.07461 2.93019C1.23864 7.16437 0.46302 11.3053 0.838165 15.3924L0.868838 15.7266L1.13844 15.9264C2.81818 17.1714 4.68053 18.1233 6.68582 18.719L7.18892 18.8684L7.50166 18.4469C7.96179 17.8268 8.36504 17.1824 8.709 16.4944L8.71099 16.4904C10.8645 17.0471 13.128 17.0485 15.2821 16.4947C15.6261 17.1826 16.0293 17.8269 16.4892 18.4469L16.805 18.8725L17.3116 18.717C19.3056 18.105 21.1876 17.1751 22.8559 15.9238L23.1224 15.724L23.1528 15.3923C23.5873 10.6524 22.3579 6.53306 19.8947 2.90714L19.7759 2.73227L19.5833 2.64518C18.1437 1.99439 16.6386 1.51826 15.0686 1.25995ZM16.6074 10.7755L16.6074 10.7756C16.5934 11.6409 16.0212 12.1444 15.4783 12.1444C14.9297 12.1444 14.3493 11.6173 14.3493 10.7877C14.3493 9.94885 14.9378 9.41192 15.4783 9.41192C16.0471 9.41192 16.6209 9.93851 16.6074 10.7755ZM8.49373 12.1444C7.94513 12.1444 7.36471 11.6173 7.36471 10.7877C7.36471 9.94885 7.95323 9.41192 8.49373 9.41192C9.06038 9.41192 9.63892 9.93712 9.6417 10.7815C9.62517 11.6239 9.05462 12.1444 8.49373 12.1444Z"
-          strokeWidth="1.5"
-        />
-      </svg>
-    ),
+    title: 'Receita pavê',
+    description: 'Pavê doce',
+    image:
+      'https://imgs.search.brave.com/sMw55zqSvOa4JYMEq_1WppdKzdTFQLUhhD_uqWzcDjM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9wYW5l/bGF0ZXJhcGlhLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/My8wOS9yZWNlaXRh/LXBhdmUtdHJhZGlj/aW9uYWwtNzU4eDUw/NS5qcGc',
+  },
+  {
+    title: 'Receita geleia',
+    description: 'Geleia caseira',
+    image:
+      'https://imgs.search.brave.com/E-627Q3CdEvTjjZB0FY5dM3OsCMiiJq5hq3A9oiuZ-8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pYmFz/c2V0cy5jb20uYnIv/aWIuc3RvcmUuYmxv/Zy9ibG9nLWI0YzUx/MjI5M2ZhNTQwY2Fh/NDRkNGRjNTE4Zjkx/MTAyLmpwZWc',
+  },
+  {
+    title: 'Receita mousse',
+    description: 'Mousse de chocolate',
+    image:
+      'https://imgs.search.brave.com/VMJdvn11P0gQ7aoeGEn_LowjoU2EWMATbudSp-LHxLA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaXRkZy5jb20u/YnIvaW1hZ2VzLzY0/MC00NDAvOGZlZDhm/NjBkM2M4ZTM5OTAz/OTZlMjQ3OGNiYzdm/MmEvc2h1dHRlcnN0/b2NrLTE5MDU2MTc1/NzUtMS0uanBn',
+  },
+  {
+    title: 'Receita torta',
+    description: 'Torta de limão',
+    image:
+      'https://imgs.search.brave.com/4-Gs_XEdi3Y7rCcbq40rlaAsK679bqJKVusd4V-JHeM/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaXRkZy5jb20u/YnIvaW1hZ2VzLzY0/MC00NDAvOWZkYjdj/OGYzOWEwMTU5YzU1/OWUzNmU2NjU2NzMz/MDEvMzE3NjM0LW9y/aWdpbmFsLmpwZw',
+  },
+  {
+    title: 'Receita salada',
+    description: 'Salada fresca',
+    image:
+      'https://imgs.search.brave.com/jwNk6LbPSN5HZ0koKV-tkr9cx52bGC3MwM4EJx8N9rQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaXRkZy5jb20u/YnIvaW1hZ2VzLzY0/MC00NDAvZDhmYTI1/NmRhMjQzMWYzNWQy/MDdjMzQ4OWUxMTk3/M2MvMzQ2MjQ0LW9y/aWdpbmFsLmpwZw',
+  },
+  {
+    title: 'Receita arroz',
+    description: 'Arroz à grega',
+    image:
+      'https://imgs.search.brave.com/ImFzQoqKvuZcER1KSolrcQd2YnpHVTpE8SyFM74Vtf0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaXRkZy5jb20u/YnIvaW1hZ2VzLzY0/MC00NDAvOGQ1ZmZi/N2RmNDFkZmQ5OTBj/ZTFhMjBiMWM0NWMz/NmYvc2h1dHRlcnN0/b2NrLTE4NDM3NDcx/Ni0xLS5qcGc',
+  },
+  {
+    title: 'Receita pizza',
+    description: 'Pizza caseira',
+    image:
+      'https://imgs.search.brave.com/2InLA928-dh-6HwH1E6Snd8HKtOv9GLmAUCzUtYM4bQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaXRkZy5jb20u/YnIvaW1hZ2VzLzY0/MC00NDAvNDdkNjU4/M2M5M2Q3N2VkYWM1/MjQ0Y2FiNjdiYTY2/MGIvMjUzNDQ3LTM3/ODIyNjc1Ni1vcmln/aW5hbC5qcGc',
+  },
+  {
+    title: 'Receita pastel',
+    description: 'Pastel de feira',
+    image:
+      'https://imgs.search.brave.com/ns4NdjjGXV63lhSfmBaTGyB-tKsQGIEn5UluoHau8vc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaXRkZy5jb20u/YnIvaW1hZ2VzLzY0/MC00NDAvM2M1ZDU0/MGJlMTYzYTY5OWJi/YmZkMTJjZTlkNDRl/NGEvMzU1MDE0LW9y/aWdpbmFsLmpwZw',
   },
 ];
+
+const ITEMS_PER_PAGE = 3;
+
+export function Welcome() {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const startIndex = currentPage * ITEMS_PER_PAGE;
+  const currentRecipes = recipes.slice(startIndex, startIndex + ITEMS_PER_PAGE);
+
+  const isFirstPage = currentPage === 0;
+  const isLastPage = startIndex + ITEMS_PER_PAGE >= recipes.length;
+
+  const goToPrevious = () => {
+    if (!isFirstPage) setCurrentPage((prev) => prev - 1);
+  };
+
+  const goToNext = () => {
+    if (!isLastPage) setCurrentPage((prev) => prev + 1);
+  };
+
+  return (
+    <div className='flex flex-col items-center justify-center gap-8 w-full px-4'>
+      <img
+        src={logoLight}
+        alt='React Router'
+        className='w-24 h-24 rounded-full flex items-center justify-center'
+      />
+
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4 w-full max-w-6xl'>
+        {/** Place here the component card*/}
+        {currentRecipes.map((recipe, idx) => (
+          <div
+            key={idx}
+            className='border-2 border-violet-500 rounded-lg p-4 flex flex-col items-center'
+          >
+            <h2 className='font-bold text-lg'>{recipe.title}</h2>
+            <p className='text-sm text-gray-600 mb-4'>{recipe.description}</p>
+            <img
+              src={recipe.image}
+              alt={recipe.title}
+              className='w-full h-48 object-cover rounded-md'
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className='flex gap-4'>
+        <Button
+          children='Anterior'
+          variant={isFirstPage ? 'tertiary': 'secondary'}
+          onClick={goToPrevious}
+          disabled={isFirstPage}
+        />
+        <Button
+          children='Próximo'
+          variant={isLastPage ? 'tertiary' : 'primary'}
+          onClick={goToNext}
+          disabled={isLastPage}
+        />
+      </div>
+    </div>
+  );
+}
