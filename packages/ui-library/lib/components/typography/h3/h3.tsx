c8c8abbe3d84d5ee4c2d9914e2a3bed38372f4c6
Lucas Nagaoka <lucasnagaoka@gmail.com>
2025-05-18 15:08:41 -0300

    fix(typography components): fixing css classes being overridden (#20)

--- packages/ui-library/lib/components/typography/h3/h3.tsx
+++ packages/ui-library/lib/components/typography/h3/h3.tsx
@@ -5,9 +5,9 @@ import { h3 } from './h3.styles.css';
 import type { JSX } from 'react';
 import type { H3Props } from './h3.types.ts';
 
-export function H3({ children, ...other }: H3Props): JSX.Element {
+export function H3({ className, children, ...other }: H3Props): JSX.Element {
   return (
-    <h3 className={clsx([fontText, h3])} {...other}>
+    <h3 className={clsx([className, fontText, h3])} {...other}>
       {children}
     </h3>
   );
